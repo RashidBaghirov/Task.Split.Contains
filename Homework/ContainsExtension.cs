92070a2b4c857c8430d38a88826a0082e3f2c7d4
RashidBaghirov <rashidtb@code.edu.az>
2022-11-27 22:11:08 +0400

    Task is very Hard

---
+++ Homework/ContainsExtension.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal static class Extension
+    {
+        public static bool CustomContains(this string sentence, string search)
+        { 
+            
+            var length = sentence.Length - search.Length;
+            if (length < 0)
+            {
+                return false;
+            }         
+                for (int i = 0; i <= length; i++) //bir alma almaya alma dedi
+                {
+                    int j;
+                    for (j = 0; j < search.Length; j++) //alma
+                        if (sentence[i + j] != search[j])
+                        {
+                            break;
+                        }
+                    if (j == search.Length)
+                    {
+                        return true;
+                    }      
+                }
+            return false;
+        }
+   
+        
+        
+        public static string[] CustomSplit(this string sentence , char symbol)
+        {
+            string symbl = " ";
+            string[] arr = new string[0];
+            for (int i = 0; i <sentence.Length; i++)
+            {
+                if (sentence[i] == symbol)
+                {
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length - 1] = symbl;
+                    symbl = string.Empty;
+                }
+                else
+                {
+                    symbl += sentence[i];
+                }
+            }
+                Array.Resize(ref arr, arr.Length + 1);
+                arr[arr.Length - 1] = symbl;
+                return arr;
+        }
+    }
+            
+
+   
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Homework;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+
+string sentence = "salam necesen";
+foreach (var item in sentence.CustomSplit(' ')) 
+{
+    Console.WriteLine(item);
+}
+
